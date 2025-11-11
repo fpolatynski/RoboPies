3df63e5e4045eb1769df043963e77436f84f7f02
Filip Połatyński <fpolatynski@gmail.com>
2025-11-11 22:22:44 +0100

    some simulation changes

--- training.m
+++ training.m
@@ -1,6 +1,6 @@
 % Generate seed
 % Check if GPU is available and set training options accordingly
-rng(960)
+rng(990)
 
 % Initialise all variables
 addpath("functions\");
@@ -13,6 +13,9 @@ mdl = "RoboPies";
 open_system(mdl)
 
 env = rlSimulinkEnv("RoboPies", "RoboPies/Agent", obsInfo, actInfo);
+%in = Simulink.SimulationInput(mdl);
+env.ResetFcn = @(in) reset_height(in);
+%env.ResetFcn = @(in) setVariable(in, 'robot_offset', 10);
 %agent = rlDDPGAgent(obsInfo, actInfo, initOptions, agentOptions);
 % Create the DDPG agent
 %agent = createTD3Agent(numObs, obsInfo, numAct, actInfo, T);
@@ -24,4 +27,6 @@ trainingStats = train(agent, env, trainOptions, Evaluator=evaluator, Logger=logg
 
 %%
 simOptions = rlSimulationOptions(MaxSteps=floor(Tf/T));
-experience = sim(env,agent,simOptions);
+experiences = sim(env,agent,simOptions);
+%%
+%agent = episodeData.Agent{1};
